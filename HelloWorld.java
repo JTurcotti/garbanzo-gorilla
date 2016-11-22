27b8468b18da1a4c5de524bf0c24d83d67595645
JTurcotti <turcotti.joshua@gmail.com>
2016-11-22 14:39:21 -0500

    Added more comments to HelloWorld.java
    we need more comments!

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,11 @@
  * This should have been commented before
  */
 public class HelloWorld {
+  /**
+    main method that performs the primary function of the class:
+    to print "Hello World"
+    This shold have been commented too
+    */
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
